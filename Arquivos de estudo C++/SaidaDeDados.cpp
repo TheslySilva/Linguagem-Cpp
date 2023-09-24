cfb1dbfc63281aa43d53ce3d5716435213404b90
Thesly Silva <97068069+TheslySilva@users.noreply.github.com>
2023-09-23 23:04:11 -0300

    update Arquivos de estudo C++/SaidaDeDados.cpp

---
+++ Arquivos de estudo C++/SaidaDeDados.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+//Variaveis
+    string nome= "Thesly Silva";
+    int idade= 21;
+    char sexo= 'M';
+    double altura = 1.61;
+
+//Saida de dados
+    cout << "Nome = " << nome << endl;
+    cout << "Idade = " << idade << endl;
+    cout << "Sexo = " << sexo << endl;
+    cout << "Altura = " << altura << endl;
+
+    return 0;
+}
