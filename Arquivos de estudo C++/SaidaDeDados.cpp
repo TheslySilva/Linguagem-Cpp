0197dea573a42d4ba5dbad605091f0a37808c30c
Thesly Silva <97068069+TheslySilva@users.noreply.github.com>
2023-09-23 23:32:22 -0300

    update Arquivos de estudo C++/SaidaDeDados.cpp

--- Arquivos de estudo C++/SaidaDeDados.cpp
+++ Arquivos de estudo C++/SaidaDeDados.cpp
@@ -1,31 +1,32 @@
 #include <iostream>//Entrada e Saida de dados
 #include <iomanip>//Formata numeros decimais
 
-using namespace std;//Biblioteca padrao
+using namespace std; //Biblioteca padrao
 
 int main() {
 
-    //Variaveis
-    string nome;
-    int idade;
-    char sexo;
-    double altura,
-           salario;
+	//Variaveis
+	string nome;
+	int idade;
+	char sexo;
+	double altura,
+	salario;
 
-    //Atribuindo valores
-    nome = "Thesly Silva";
-    idade = 21;
-    sexo = 'M';
-    altura = 1.61;
-    salario = 394.632;
+	//Atribuindo valores
+	nome = "Thesly Silva";
+	idade = 21;
+	sexo = 'M';
+	altura = 1.61;
+	salario = 394.632;
 
-    //Saida de dados
-    cout << fixed << setprecision(2);
-    cout << "Nome = " << nome << endl;
-    cout << "Idade = " << idade << endl;
-    cout << "Sexo = " << sexo << endl;
-    cout << "Altura = " << altura << endl;
-    cout << "Salario = " << salario << endl;
-    cout << "\nA pessoa " << nome << " sexo " << sexo << "tem "<< idade << altura << "e ganha " << salario << endl;
-    return 0;
+	//Saida de dados
+	cout << fixed << setprecision(2);
+	cout << "Nome = " << nome << endl;
+	cout << "Idade = " << idade << endl;
+	cout << "Sexo = " << sexo << endl;
+	cout << "Altura = " << altura << endl;
+	cout << "Salario = " << salario << endl;
+	cout << "\nA pessoa " << nome << " sexo " << sexo << "tem "<< idade << altura << "e ganha " << salario << endl;
+	
+	return 0;
 }
