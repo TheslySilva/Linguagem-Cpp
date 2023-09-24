1e64c7e6e597a888ad739c7ee9674e9a3fbbd33e
Thesly Silva <97068069+TheslySilva@users.noreply.github.com>
2023-09-23 23:19:03 -0300

    update Arquivos de estudo C++/SaidaDeDados.cpp

--- Arquivos de estudo C++/SaidaDeDados.cpp
+++ Arquivos de estudo C++/SaidaDeDados.cpp
@@ -1,30 +1,31 @@
 #include <iostream>//Entrada e Saida de dados
 #include <iomanip>//Formata numeros decimais
 
-using namespace std;
+using namespace std;//Biblioteca padrao
 
 int main() {
 
 	//Variaveis
-  string nome;
-  int idade;
-  char sexo;
-  double altura,salario;
-  
-  //Atribuindo valores
+	string nome;
+	int idade;
+	char sexo;
+	double altura,
+	salario;
+
+	//Atribuindo valores
 	nome = "Thesly Silva";
 	idade = 21;
 	sexo = 'M';
 	altura = 1.61;
-    salario= 394.632;
+	salario = 394.632;
 
 	//Saida de dados
-    cout << fixed << setprecision(2);
+	cout << fixed << setprecision(2);
 	cout << "Nome = " << nome << endl;
 	cout << "Idade = " << idade << endl;
 	cout << "Sexo = " << sexo << endl;
 	cout << "Altura = " << altura << endl;
-    cout << "Salario = " << salario << endl;
+	cout << "Salario = " << salario << endl;
 
 	return 0;
 }
