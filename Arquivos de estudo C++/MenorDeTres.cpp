6500f8c54ae494f8a53ef115acfd437e615a8d4e
Thesly Silva <97068069+TheslySilva@users.noreply.github.com>
2023-09-24 09:00:10 -0300

    update Arquivos de estudo C++/MenorDeTres.cpp

--- Arquivos de estudo C++/MenorDeTres.cpp
+++ Arquivos de estudo C++/MenorDeTres.cpp
@@ -2,26 +2,29 @@
 
 using namespace std;
 
-int main(){
-	
-	int n1,n2,n3,n4;
-	
-	cout << "Digite o primeiro numero : " <<endl;
-	cin >> n1 ;
-	cout << "Digite o segundo numero : " <<endl;
-	cin >> n2 ;
-	cout << "Digite o terceiro numero : " <<endl;
-	cin >> n3 ;
-	
-	if(n1<n2 && n1<n3){
-		n4=n1;
-	}else if(n2<n1 && n2<n3){
-		n4=n2;
-	}else if(n3<n1 && n3<n2){
-		n4=n3;
-	}else{
-		n4=n1;
-	}
-	
-	return 0;
+int main() {
+
+    int n1,
+        n2,
+        n3,
+        n4;
+
+    cout << "Digite o primeiro numero : " ;
+    cin >> n1;
+    cout << "Digite o segundo numero : " ;
+    cin >> n2;
+    cout << "Digite o terceiro numero : " ;
+    cin >> n3;
+
+    if(n1 < n2 && n1 < n3) {
+        n4 = n1;
+    } else if(n2 < n1 && n2 < n3) {
+        n4 = n2;
+    } else if(n3 < n1 && n3 < n2) {
+        n4 = n3;
+    } else {
+        n4 = n1;
+    }
+    cout << "\nMENOR = " << n4<< endl;
+    return 0;
 }
