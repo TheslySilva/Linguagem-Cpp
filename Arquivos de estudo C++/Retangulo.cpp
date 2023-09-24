bb4a785f8e8df4517bc8660bce0e9eb225668c32
Thesly Silva <97068069+TheslySilva@users.noreply.github.com>
2023-09-24 00:39:55 -0300

    update Arquivos de estudo C++/Retangulo.cpp

--- Arquivos de estudo C++/Retangulo.cpp
+++ Arquivos de estudo C++/Retangulo.cpp
@@ -6,21 +6,26 @@ using namespace std;
 
 int main() {
 
-    double base,altura,area,perimetro,diagonal;
+	double base,
+	altura,
+	area,
+	perimetro,
+	diagonal;
 
-    cout << "Base do retangulo : ";
-    cin >> base;
-    cout << "Altura do retangulo : ";
-    cin >> altura;
-    
-    cout << fixed << setprecision(4);
-    
-    area = base * altura;
-    perimetro= 2*(base+altura);
-    diagonal= sqrt(pow(base,2) + pow(altura,2));
+	cout << "Base do retangulo : ";
+	cin >> base;
+	cout << "Altura do retangulo : ";
+	cin >> altura;
 
-    cout << "\nAREA = " << area<<endl;
-    cout << "PERIMETRO = "<<perimetro << endl;
-    cout << "DIAGONAL = " <<diagonal<< endl;
-    return 0;
+	cout << fixed << setprecision(4);
+
+	area = base * altura;
+	perimetro = 2*(base+altura);
+	diagonal = sqrt(pow(base, 2) + pow(altura, 2));
+
+	cout << "\nAREA = " << area<<endl;
+	cout << "PERIMETRO = "<<perimetro << endl;
+	cout << "DIAGONAL = " <<diagonal<< endl;
+	
+	return 0;
 }
